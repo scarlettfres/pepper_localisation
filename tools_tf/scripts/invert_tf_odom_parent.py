428efaeec5350a42b590b00a1e5f8f53e5bf4770
scarlettfres <scarlett_fres@hotmail.com>
2015-08-28 18:30:23 +0200

    ok nouveau bridge recup de lodom via almotion

--- tools_tf/scripts/invert_tf_odom_parent.py
+++ tools_tf/scripts/invert_tf_odom_parent.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 import roslib
 import rospy
+from sensor_msgs.msg import JointState
 import tf
+from naoqi import ALProxy
 import geometry_msgs.msg
 from tf.msg import tfMessage
-from tf.transformations import quaternion_inverse, quaternion_conjugate
+import sys
+from tf.transformations import (quaternion_inverse,
+                                quaternion_conjugate,
+                                quaternion_from_euler)
 
 """
 when launching pepper, odom is the begining of the tf tree. We want
@@ -15,28 +20,42 @@ modify the t tree as we want
 """
 
 
-def callback(data):
-    broadcaster = tf.TransformBroadcaster()
-    trans = [data.transforms[0].transform.translation.x,
-             data.transforms[0].transform.translation.y,
-             data.transforms[0].transform.translation.z]
-    rot = [data.transforms[0].transform.rotation.x,
-           data.transforms[0].transform.rotation.y,
-           data.transforms[0].transform.rotation.z,
-           data.transforms[0].transform.rotation.w
-           ]
+class publish_odom:
 
-    # euler = euler_from_quaternion(rot)
-    # rot_fin = quaternion_from_euler(-euler[0], -euler[1], -euler[2])
+    """
+    contains all services that can be called by the client
+    """
 
-    rot_bis = quaternion_conjugate(rot)
-    broadcaster.sendTransform(trans, rot, rospy.Time.now(),
-                              data.transforms[0].header.frame_id,
-                              data.transforms[0].child_frame_id)
+    def __init__(self, ip):
+        self.motionProxy = ALProxy("ALMotion", ip, 9559)
+        self.broadcaster = tf.TransformBroadcaster()
+        rospy.Subscriber("/joint_states", JointState, self.callback)
+
+    def callback(self, data):
+
+        position = self.motionProxy.getPosition("Torso", 1, True)
+
+        # trans = [data.transforms[0].transform.translation.x,
+        #          data.transforms[0].transform.translation.y,
+        #          data.transforms[0].transform.translation.z]
+        # rot = [data.transforms[0].transform.rotation.x,
+        #        data.transforms[0].transform.rotation.y,
+        #        data.transforms[0].transform.rotation.z,
+        #        data.transforms[0].transform.rotation.w
+        #        ]
+
+        # euler = euler_from_quaternion(rot)
+        trans = (position[0], position[1], position[2])
+        rot = quaternion_from_euler(position[3], position[4], position[5])
+
+        # rot_bis = quaternion_conjugate(rot)
+        self.broadcaster.sendTransform(trans, rot, rospy.Time.now(),
+                                       "/odom_bis",
+                                       "/base_link")
 
 if __name__ == '__main__':
     rospy.init_node('invert_tf_odom_parent')
-    rospy.Subscriber("tf_relay", tfMessage, callback)
+    publish_odom(sys.argv[1])
     try:
         rospy.spin()
     except KeyboardInterrupt:
